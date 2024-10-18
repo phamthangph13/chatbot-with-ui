cb003e3c652d5fd14c85f57f1dfa9a2c3baee4fa
PhamThang <winnieph13@gmail.com>
2024-10-18 13:25:18 +0700

    change to localhost api

--- backend.js
+++ backend.js
@@ -32,7 +32,7 @@ function addMessage(sender, message) {
 
 async function sendMessageToChatbot(message) {
     try {
-        const response = await fetch('http://127.0.0.1:5000/chat', {
+        const response = await fetch('https://localhost:5000/chat', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
